89eafdeddc269bd839bcff1431e89420bf86225f
Giacomo Materozzi <mate.giacomo@gmail.com>
2020-05-06 15:52:52 +0200

    rewrited slider logic, removed stored offset and enabled the "value" property to decide position of knob

--- AF/elements/Slider/Slider.component.js
+++ AF/elements/Slider/Slider.component.js
@@ -18,11 +18,13 @@ const {
   round,
   add,
   set,
-  diffClamp,
   concat,
   divide,
   block,
   onChange,
+  max,
+  min,
+  interpolate,
 } = Animated;
 
 type Props = {
@@ -31,74 +33,49 @@ type Props = {
   onChange: number => void,
   min: number,
   max: number,
-  default: number,
+  value: number,
   color?: string,
 };
 
-const withOffset = (state, value, defaultOffset) => {
-  const offset = new Value(defaultOffset);
-  return block([
-    cond(
-      eq(state, State.END),
-      [set(offset, add(value, offset)), offset],
-      add(value, offset),
-    ),
-  ]);
-};
-
-// for resolve bug in diffClamp follow this pull request https://github.com/software-mansion/react-native-reanimated/pull/746
 const Slider = (props: Props) => {
-  const {state, translationX} = useMemo(
-    () => ({
-      state: new Value(State.UNDETERMINED),
-      translationX: new Value(0),
-    }),
-    [],
-  );
+  const state = useMemo(() => new Value(State.UNDETERMINED), []);
+  const translationX = useMemo(() => new Value(0), []);
 
   const withPercentualeDefault =
-    (props.width / (props.max - props.min)) * (props.default - props.min);
-
-  const offset = useMemo(() => withPercentualeDefault, []);
+    (props.width / (props.max - props.min)) * (props.value - props.min);
 
   const gestureHandler = onGestureEvent({
     state,
     translationX,
   });
 
-  const translateX = useMemo(
-    () =>
-      sub(
-        diffClamp(withOffset(state, translationX, offset), 0, props.width),
-        KNOB_WIDTH / 2,
-      ),
-    [],
+  const translateX = min(
+    max(add(translationX, withPercentualeDefault), 0),
+    props.width,
   );
 
-  const percentage = useMemo(
-    () =>
-      round(
-        add(
-          multiply(
-            divide(sub(props.max, props.min), props.width),
-            add(translateX, divide(KNOB_WIDTH, 2)),
-          ),
-          props.min,
-        ),
-      ),
-    [],
+  const scaleX = interpolate(add(translateX), {
+    inputRange: [0, props.width],
+    outputRange: [0, 1],
+  });
+
+  const percentage = round(
+    add(
+      multiply(divide(sub(props.max, props.min), props.width), translateX),
+      props.min,
+    ),
   );
 
-  const label = useMemo(() => concat(percentage), []);
+  const label = concat(percentage);
 
   useCode(
     () =>
       block([
-        call([translateX], ([]) => {}),
         onChange(
           state,
           cond(eq(state, State.END), [
             set(state, State.UNDETERMINED),
+            set(translationX, 0),
             call([percentage], ([percentageChanged]) => {
               props.onChange && props.onChange(percentageChanged);
             }),
@@ -111,10 +88,25 @@ const Slider = (props: Props) => {
   return (
     <View style={[styles.container, props.style, {width: props.width}]}>
       <View style={styles.backgroundSlide}>
-        <Animated.View style={[styles.left]} />
+        <Animated.View
+          style={[
+            styles.left,
+            {
+              backgroundColor: props.color,
+              transform: [
+                {translateX: -props.width / 2},
+                {scaleX},
+                {translateX: props.width / 2},
+              ],
+            },
+          ]}
+        />
       </View>
       <PanGestureHandler {...gestureHandler}>
-        <Animated.View style={{transform: [{translateX}]}}>
+        <Animated.View
+          style={{
+            transform: [{translateX: sub(translateX, KNOB_WIDTH / 2)}],
+          }}>
           <Knob state={state}>
             <ReText text={label} style={styles.label} />
           </Knob>
