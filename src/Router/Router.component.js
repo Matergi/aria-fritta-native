dd23230004782b1e25a3a63d95cfa3abcdcb0781
Giacomo Materozzi <mate.giacomo@gmail.com>
2020-06-08 12:44:36 +0200

    updated animation of router, the animation from right, and added the default transition

--- src/Router/Router.component.js
+++ src/Router/Router.component.js
@@ -2,6 +2,13 @@
 import React from 'react';
 import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 import routerScreens from './config.router';
+import {fromRight} from './transitions';
+import {Platform} from 'react-native';
+
+const SYSTEM_ANIMATION = undefined;
+
+const DEFAULT_ANIMATION_ANDROID = fromRight;
+const DEFAULT_ANIMATION_IOS = SYSTEM_ANIMATION;
 
 export default (defaultScreen: string) => {
   const ScreenStack = createSharedElementStackNavigator();
@@ -15,10 +22,18 @@ export default (defaultScreen: string) => {
           key={screen.router.id}
           name={screen.router.id}
           component={screen.ui}
-          options={screen.navigationOptions}
+          options={
+            screen.navigationOptions
+              ? screen.navigationOptions
+              : Platform.OS === 'android'
+              ? DEFAULT_ANIMATION_ANDROID
+              : DEFAULT_ANIMATION_IOS
+          }
           sharedElementsConfig={screen.sharedElementsConfig}
         />
       ))}
     </ScreenStack.Navigator>
   );
 };
+
+export const durationTransition = 200;
